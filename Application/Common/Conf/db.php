b7ce9073611282bb6b5c18e8c768b1bda5b2b396
Biny <1559867170@qq.com>
2019-05-11 14:08:15 +0800

    Update db.php

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -2,11 +2,11 @@
 return array(
 
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_USER' => 'root',
-    'DB_PWD' => '',
+    'DB_HOST' => 'mysql.coding.io',
+    'DB_USER' => 'user-GmL9ZS1fmw',
+    'DB_PWD' => 'tPBGDJCpyif?oWx9O(2a',
     'DB_PORT' => 3306,
-    'DB_NAME' => 'imooc_singcms',
+    'DB_NAME' => 'db-NIX7mpSfYx',
     'DB_CHARSET' => 'utf8',
     'DB_PREFIX' =>'cms_',
 );
